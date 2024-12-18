d214f358839cda75fdf87408943e0481ab0e1490
Muhammad-badhusha <muhammadbadhusha7@123>
2024-12-18 05:45:08 -0800

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Calculator from './calculator'
-import{BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import{HashRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
  
 
